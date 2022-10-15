7b8627fb6906d45e07537d8750ae4fb80d86090b
Anastasia Volzhina <happy-heart@bk.ru>
2022-10-15 19:58:12 +0300

    fix2

--- app.js
+++ app.js
@@ -34,12 +34,6 @@ app.use(
 
 app.use(cookieParser());
 
-// app.get('/crash-test', () => {
-//   setTimeout(() => {
-//     throw new Error('Сервер сейчас упадёт');
-//   }, 0);
-// });
-
 app.use(requestLogger);
 
 app.get('/logout', logout);
