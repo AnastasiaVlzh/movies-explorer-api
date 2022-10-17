0fea3085decce9302638274c6a785ac2dfbab17a
Anastasia Volzhina <happy-heart@bk.ru>
2022-10-17 22:56:09 +0300

    fix after review

--- routes/index.js
+++ routes/index.js
@@ -18,10 +18,6 @@ router.post('/signin', express.json(), celebrate({
 router.post('/signup', express.json(), celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi
-      .string()
-      .regex(/^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*\/?$/),
     email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
