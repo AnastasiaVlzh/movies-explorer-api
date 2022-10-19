0f09c395993d6e26f9fa974f9bee1aee9f95104d
Anastasia Volzhina <happy-heart@bk.ru>
2022-10-19 18:33:53 +0300

    fix after second review

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const helmet = require('helmet');
 const { errors } = require('celebrate');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const routes = require('./routes/index');
+const { errorsGlobal } = require('./middlewares/errorsGlobal');
 
 const {
   NODE_ENV,
@@ -27,6 +29,7 @@ app.use(
     credentials: true,
   }),
 );
+app.use(helmet());
 
 app.use(cookieParser());
 
@@ -38,13 +41,7 @@ app.use(errorLogger);
 
 app.use(errors());
 
-app.use((err, req, res, next) => {
-  const statusCode = err.statusCode || 500;
-  const message = statusCode === 500 ? 'На сервере произошла ошибка' : err.message;
-
-  res.status(statusCode).send({ message });
-  next();
-});
+app.use(errorsGlobal);
 
 async function main() {
   try {
