6e4c6b90fe61cd966ac3bc9f090e59842527a1c4
Anastasia Volzhina <happy-heart@bk.ru>
2022-11-23 18:33:50 +0300

    get movies

--- controllers/movie.js
+++ controllers/movie.js
@@ -44,8 +44,9 @@ module.exports.createMovie = async (req, res, next) => {
 };
 
 module.exports.getMovies = async (req, res, next) => {
+  const owner = req.user._id;
   try {
-    const movie = await Movie.find({});
+    const movie = await Movie.find({ owner });
     return res.status(200).send(movie);
   } catch (err) {
     return next(new ServerError('Произошла ошибка'));
