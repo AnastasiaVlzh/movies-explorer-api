ed890449f059c5a1df86655483997ef3ed014861
Anastasia Volzhina <happy-heart@bk.ru>
2022-11-26 15:57:08 +0300

    адрес фронта

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000', 'https://anastasiavlzh-diploma.nomoredomains.icu'],
     credentials: true,
   }),
 );
